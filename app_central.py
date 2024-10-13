29801139e787dd1c01f76aaa60ab01d02e20b7ac
Christine Guo <67035166+christine-guo@users.noreply.github.com>
2024-10-13 00:00:25 -0400

    Update app_central.py

--- app_central.py
+++ app_central.py
@@ -1,31 +1,49 @@
 import requests
 from flask import Flask, request
+from concurrent.futures import ThreadPoolExecutor
 
 app = Flask(__name__)
 
-# List of all VM URLs [MUST DO THIS MANUALLY EVERY TIME SPIN UP NEW VMs]
+# List of all VM URLs [need to manually edit]
 all_vms = [
-  "http://35.227.30.251:5000",
-  "http://35.192.81.17:5000",
-  "http://35.197.104.168:5000",
-  "http://34.38.225.117:5000",
-  "http://35.228.48.60:5000",
-  "http://104.155.214.65:5000",
-  "http://34.87.87.112:5000",
+  "http://34.75.246.52:5000",
+  "http://34.171.228.132:5000",
+  "http://34.83.63.223:5000",
+  "http://34.140.35.230:5000",
+  "http://34.88.225.17:5000",
+  "http://35.221.197.15:5000",
+  "http://35.185.188.182:5000",
 ]
 
+# sends a http request to validate the domain at the given VM
+def send_request(vm, domain, token):
+    response = requests.post(f"{vm}/validate", json={"domain": domain, "token": token}, timeout = 30)
+    return vm, response.status_code
+    
+
+# function to begin running DCV at all 7 GCP perspectives 
 @app.route('/run-all', methods=['POST'])
 def runAll():
     data = request.json
     domain = data.get('domain')
     token = data.get('token')
 
-    # Send requests to other VMs
-    for vm in other_vms:
-        requests.post(f"{vm}/validate", json={"domain": domain, "token": token})
+    errors = []
 
-    return {"message": "All validated"}, 200
+    # send DCV requests to all VMs and wait for their completion
+    with ThreadPoolExecutor() as executor:
+        futures = {executor.submit(send_request, vm, domain, token): vm for vm in all_vms}
+        for future in futures:
+            vm, status_code = future.result()  
+            if status_code != 200:
+                errors.append(f"{vm} failed with status code {status_code}")
+    
+    if errors:
+        return {"errors": errors, "message": "DCV failed at some perspectives"}
+    else:
+        return {"message": "DCV completed at all 7 GCP perspectives!"}
 
+# function to run DCV at the current perspective and return a response when done 
 @app.route('/validate', methods=['POST'])
 def validate():
     data = request.json
@@ -35,9 +53,11 @@ def validate():
     # ping the domain through an http get request 
     try: 
         response = requests.get(f"http://{domain}/?token={token}", timeout=30)
-        return response.text    
-    except requests.RequestException:
-        return None  # Return None (null in JSON) if the request fails or times out
+        response.raise_for_status() 
+        return {"status_code": response.status_code}, 200 
+    except requests.RequestException as e:
+        return {"error": str(e)}, 400
+    
 
 
 if __name__ == '__main__':
